5935124f7a2c343362a1cf1a114c0849c881a192
Étienne Morice <pro.emorice@mail.com>
2018-03-29 21:00:08 +0200

    Fixing regression on solar system
    (That one is so coool ! Well done mate !)

--- src/n_bodies/Tester.java
+++ src/n_bodies/Tester.java
@@ -18,6 +18,7 @@ import lockfree.Visualizer;
 
 import systems.PSystem;
 import systems.GridSystem;
+import systems.SolarSystem;
 
 public class Tester {
 
@@ -89,63 +90,27 @@ public class Tester {
 		engine.join();
 	}
 	
-	/*
 	public static void testSolarSystem(int nThreads, int bufferSize, int maxTime, int n){
-		
-		//v= sqrt(GM/r) but here G = 1
-		final int sunRadius = 50;
-		final float sunMass = 1000.0f;
-		final int planetRadius=10;
-		final float planetMass = 10.0f;
-		
 		delta = 1.0f;
-		bodies = new Body[n];
-		clock = new Clock();
-		threads = new Thread[nThreads];
-		force = new GravitationnalForce();
-		int xBegin = WIDTH/2 - ( sunRadius)/2 ;
-		int yBegin = HEIGHT/2 - (sunRadius)/2 ;
-		boolean[][] isNegligible = new boolean[n][n];
-
-		int fillTime = 50;
-
-		bodies[0] = new Body(0,0, sunMass, sunRadius, new Vector(xBegin, yBegin), new Vector(0,0), new Vector(0,0), n);
-
-		for(int i=1; i< n ; i++){
-			double r = (Math.random() * (HEIGHT/2-sunRadius-planetRadius)) + sunRadius + planetRadius;
-			double teta = (Math.random()-0.5) * 2 * Math.PI;
-			float x =(float) (xBegin + r*Math.cos(teta));
-			float y =(float) (yBegin + r*Math.sin(teta));
-			float realD = (float) Math.sqrt(sunMass/r);
-
-			float vx =(float) (-Math.sin(teta)*realD);
-			float vy =(float) (Math.cos(teta)*realD);
-
-			bodies[i] = new Body(0,i, planetMass, planetRadius, new Vector(x, y), new Vector(vx,vy), new Vector(0,0), n);
-		}
-
-		buffer = new BlockingBuffer(bufferSize,n,bodies );
-
-		SafeCounter counter = new SafeCounter(0);
 		
-		for(int i=0; i< nThreads; i++){
-			int first = (i * n) / nThreads;
-			threads[i] = new Thread(new ProcessingNode(counter, clock, first, force, delta, buffer, maxTime, isNegligible, fillTime));
-			threads[i].start();
-		}
+		PSystem system = new SolarSystem(n, WIDTH, HEIGHT);
 
-		Thread negligibleNode = new Thread(new NegligibleNode( (float)0.95, clock, maxTime, isNegligible, buffer, fillTime));
-		negligibleNode.start();
+		Buffer buffer = new BlockingBuffer(bufferSize,n);
+		system.initBuffer(buffer);
 
+		Engine engine  = new LockfreeEngine(system, buffer, nThreads, delta, maxTime);
+
+		engine.start();
 		Visualizer visualizer = new Visualizer(delta, maxTime, 20.0f, buffer, WIDTH, HEIGHT);
+		engine.join();
+
 	}
-	*/
 
 	public static void main(String[] args){
 		//test2b(1,1000,500);
 		//testGrid(2,10000,10,2,1);
-		testGrid(2,1000,1000,15,15);
-		// testSolarSystem(2,1000,500,30);
+		//testGrid(2,1000,1000,15,15);
+		testSolarSystem(2,1000,500,30);
 	}
 
 
---
+++ src/systems/SolarSystem.java
@@ -0,0 +1,39 @@
+
+package systems;
+
+import etc.Vector;
+import etc.Body;
+import etc.GravitationnalForce;
+
+public class SolarSystem extends PSystem {
+	final int sunRadius = 50;
+	final float sunMass = 1000.0f;
+	final int planetRadius=10;
+	final float planetMass = 10.0f;
+
+	public SolarSystem(int n, int displayWidth, int displayHeight) {
+		this.n = n;
+		bodies = new Body[n];
+		force = new GravitationnalForce();
+
+		int xBegin = displayWidth/2 - ( sunRadius)/2 ;
+		int yBegin = displayHeight/2 - (sunRadius)/2 ;
+
+
+		bodies[0] = new Body(0,0, sunMass, sunRadius, new Vector(xBegin, yBegin), new Vector(0,0), new Vector(0,0), n);
+
+		for(int i=1; i< n ; i++){
+			double r = (Math.random() * (displayHeight/2-sunRadius-planetRadius)) + sunRadius + planetRadius;
+			double teta = (Math.random()-0.5) * 2 * Math.PI;
+			float x =(float) (xBegin + r*Math.cos(teta));
+			float y =(float) (yBegin + r*Math.sin(teta));
+			float realD = (float) Math.sqrt(sunMass/r);
+
+			float vx =(float) (-Math.sin(teta)*realD);
+			float vy =(float) (Math.cos(teta)*realD);
+
+			bodies[i] = new Body(0,i, planetMass, planetRadius, new Vector(x, y), new Vector(vx,vy), new Vector(0,0), n);
+		}
+	}
+}
+
